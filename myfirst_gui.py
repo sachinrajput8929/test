9a67fbbaef4fdd3a18c64d46f84788f739143d03
sachinrajput8929 <sachinrajput8929@gmail.com>
2023-10-11 11:48:23 +0530

    first

---
+++ myfirst_gui.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+root = Tk()
+root.title("GUI2")
+root.geometry("500x500")
+ 
+root.wm_iconbitmap("icon.ico")
+root.resizable(False,False)
+
+
+# lbl1=Label1(root,)
+
+
+
+root.mainloop()
+  
